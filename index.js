a5c2ac17b6ac29f186e4b49cf20a7f7215f504b0
Oleh_Volkov <oleg.volkov@existek.com>
2022-06-06 02:18:59 +0300

    main scenario was changed

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const telegraf = require('telegraf').Telegraf;
-const Sequalize = require("./db")
-const { Markup, Telegram, Telegraf } = require('telegraf');
-const dialog = require('./answers.json');
-const userModel = require('./model');
-require('dotenv').config();
+import 'dotenv/config';
+import { Markup, Telegraf } from 'telegraf';
+import dialog from './answers.js';
+// import  Sequalize from "./db";
+// import userModel from './model';
 
 let language;
 let messageCounter = 0;
-let chatId;
 let user;
+let chatId
 let timer = 90;
-let hideButton = false;
-let messages = [];
-let nonCyrillicLanguageRegex = new RegExp(/a-zA-Z/g)
-
-const bot = new telegraf(process.env.BOT_TOKEN)
+let nonCyrillicLanguageRegex = new RegExp(/a-zA-Z/g);
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start(startAction);
+bot.telegram.setChatMenuButton({ chat_id: chatId, menu_button: "restart" });
+// bot.restart((e) => { console.log(e) });
+bot.on("message", onMessageForCollaborant.bind(this));
 bot.action("ua", uaAction.bind(this));
 bot.action("ru", ruAction.bind(this));
-bot.action("agree", () => { });
-bot.action("disagree", msg => { msg.deleteMessage() })
-bot.action("finish", clearMessage.bind(this));
-bot.action("delete", clearMessage.bind(this));
-bot.on("message", onMessageForCollaborant.bind(this));
-bot.on("callback_query", (e) => { console.log(e) });
+bot.action("finish", endChatSendAdvise);
+bot.action("forward", forward);
+
 
 // security
 // TODO input regexp control, 
@@ -38,49 +34,68 @@ async function startAction(msg) {
         last_name: msg.message.from.last_name,
         username: msg.message.from.username,
     }
-    messages.push(msg);
-    messages.push(msg.update.message.message_id);
+    chatId = msg.chat.id;
+    console.log(chatId)
     await msg.reply(dialog.language.general, Markup.inlineKeyboard([
         [
             Markup.button.callback(dialog.language.positions.ua, "ua"),
             Markup.button.callback(dialog.language.positions.ru, "ru")
         ]
     ]));
-    await msg.deleteMessage();
 }
 
 async function onMessageForCollaborant(msg) {
     try {
         messageCounter++
         if (messageCounter > 2) {
-            // warning user about spam
+            return stopMessaging(msg);
         }
-        messages.push(msg);
 
         await msg.replyWithHTML(dialog.thanks[language], Markup.inlineKeyboard([
             [
-                Markup.button.callback(dialog.endChat[language], "finish")
+                Markup.button.callback(dialog.endChat[language], "finish"),
+                Markup.button.callback(dialog.forward[language], "forward")
             ]
         ]));
-        await msg.deleteMessage();
     } catch (e) {
         console.log(e)
     }
 
 }
 
+async function forward(msg) {
+    try {
+        msg.reply(dialog.forward_msg[language] + (3 - messageCounter).toString());
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+async function endChatSendAdvise(msg) {
+    try {
+        await msg.reply(dialog.advise[language]);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function stopMessaging(msg) {
+    try {
+        if (messageCounter > 2) {
+            await msg.reply(
+                dialog.warningMessageLimit[language]
+            );
+            await endChatSendAdvise(msg);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+};
+
 async function uaAction(msg) {
     try {
         language = "ua";
-        messages.push(msg);
-        await msg.answerCbQuery();
-        await msg.replyWithHTML(dialog.set_collaborant[language], Markup.inlineKeyboard([
-            [
-                Markup.button.callback(dialog.agree[language], "agreement"),
-                Markup.button.callback(dialog.disagree[language], "disagree")
-            ]
-        ]));
-        await msg.deleteMessage();
+        await msg.reply(dialog.set_collaborant[language]);
     } catch (e) {
         console.log(e)
     }
@@ -89,24 +104,7 @@ async function uaAction(msg) {
 async function ruAction(msg) {
     try {
         language = "ru";
-        messages.push(msg);
-        await msg.answerCbQuery();
-        await msg.replyWithHTML(dialog.set_collaborant[language], Markup.inlineKeyboard([
-            [
-                Markup.button.callback(dialog.agree[language], "agreement"),
-                Markup.button.callback(dialog.disagree[language], "disagree")
-            ]
-        ]));
-        await msg.deleteMessage();
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-async function clearMessage(msg) {
-    try {
-        hideButton = true;
-        await msg.deleteMessage();
+        msg.reply(dialog.set_collaborant[language]);
     } catch (e) {
         console.log(e);
     }
