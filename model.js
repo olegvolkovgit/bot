0ac676850e62c6c4c9326e070e22afe9b839254b
Oleh_Volkov <oleg.volkov@existek.com>
2022-07-06 00:45:13 +0300

    refactoring

--- model.js
+++
@@ -1,9 +0,0 @@
-const sequielize = require("./db");
-const { DataTypes } = require(sequielize);
-
-const user = sequielize.define(user, {
-    id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
-    chatId: { type: DataTypes.STRING, unique: true },
-    message: { Type: DataTypes.STRING },
-});
-
